9363eb8a1e3993e84321ea206af996fb2ff67b75
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-05-20 16:38:46 -0700

    removed-dulpicate-vars

--- bcorganizer/src/components/mainpage.js
+++ bcorganizer/src/components/mainpage.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
 import Login from "./Login";
 
-
-import Login from "./Login";
 import ButtonAppBar from '../components/Appbar';
-
-
-import Login from "./Login";
-import ButtonAppBar from '../components/Appbar';
-
 
 class MainPage extends Component {
   constructor(props) {
