e2c473f36d5c2eebc5d095b9d444ebebd783de77
moddamatt <echomatt95@gmail.com>
2019-05-21 16:23:35 -0500

    Getting user data begun and card being templated

--- bcorganizer/src/actions/index.js
+++ bcorganizer/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { axiosWithAuth } from "../components/authentification/axiosWithAuth";
 
 //********************Action for Login ****************/
 
@@ -9,14 +10,17 @@ export const POST_FAIL = "POST_FAIL";
 export const submitLogin = loginData => dispatch => {
   dispatch({ type: POST_START });
   axios
-    .post("https://business-cards-organizer-ls.herokuapp.com/api/auth/login", loginData)
+    .post(
+      "https://business-cards-organizer-ls.herokuapp.com/api/auth/login",
+      loginData
+    )
     .then(res => {
       console.log(res);
-      localStorage.setItem('token', res.data.token)
+      localStorage.setItem("token", res.data.token);
       dispatch({
         type: POST_START,
         payload: res.data
-      })
+      });
     })
     .catch(err => console.log(err.status));
 };
@@ -28,21 +32,45 @@ export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
 
 export const submitRegister = registerData => dispatch => {
-  dispatch({ type: REGISTER_START })
-  axios.post('https://business-cards-organizer-ls.herokuapp.com/api/auth/register', registerData)
+  dispatch({ type: REGISTER_START });
+  axios
+    .post(
+      "https://business-cards-organizer-ls.herokuapp.com/api/auth/register",
+      registerData
+    )
     .then(res => {
-      console.log(res)
-      localStorage.setItem('token', res.data.token)
+      console.log(res);
+      localStorage.setItem("token", res.data.token);
       dispatch({
         type: REGISTER_SUCCESS,
         payload: res.data
-      })
+      });
     })
     .catch(err => {
-      console.log(err)
+      console.log(err);
       dispatch({
         type: REGISTER_FAIL,
         payload: err.status
-      })
+      });
+    });
+};
+
+//******************** Action for Getting User Data ****************/
+
+export const FETCH_USERDATA_START = "FETCH_USERDATA_START";
+export const FETCH_USERDATA_SUCCESS = "RFETCH_USERDATA_SUCCESS";
+export const FETCH_USERDATA_FAIL = "FETCH_USERDATA_FAIL";
+
+export const getUserData = () => dispatch => {
+  dispatch({ type: FETCH_USERDATA_START });
+  axiosWithAuth()
+    .get("https://business-cards-organizer-ls.herokuapp.com/api/user")
+    .then(res => {
+      console.log(res.data);
+      dispatch({ type: FETCH_USERDATA_SUCCESS, payload: res.data });
     })
-}
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: FETCH_USERDATA_FAIL, payload: err });
+    });
+};
--- bcorganizer/src/components/Loggedin.js
+++ bcorganizer/src/components/Loggedin.js
@@ -1,1 +1,73 @@
-import react from 'react';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { connect } from "react-redux";
+import { getUserData } from "../actions";
+
+const styles = theme => ({
+  card: {
+    justifyContent: "center",
+    width: "85%",
+    display: "flex"
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  content: {
+    flex: "1 0 auto"
+  },
+  cover: {
+    width: 151
+  }
+});
+
+class Loggedin extends React.Component {
+    componentDidMount() {
+        this.props.getUserData();
+    }
+
+  render() {
+    const { classes } = this.props;
+    console.log(this.props);
+    return (
+      <div>
+        <div>
+          <Card className={classes.card}>
+            <div className={classes.details}>
+              <CardContent className={classes.content}>
+                <Typography component="h5" variant="h5">
+                  {this.props.firstname}
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                  {this.props.lastname}
+                </Typography>
+              </CardContent>
+              <div className={classes.controls}>info</div>
+            </div>
+            {/* <CardMedia
+        className={classes.cover}
+        image="/static/images/cards/live-from-space.jpg"
+        title="Live from space album cover"
+      /> */}
+          </Card>
+        </div>
+        <div />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    firstname: state.user.first_name
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getUserData }
+)(withStyles(styles)(Loggedin));
